ae4953e89b480cb9de8c9af67d3d501ea05a4b7c
Nigel Kukard <nkukard@lbsd.net>
2020-09-10 05:40:13 +0000

    tests: Migrated tests using _test_configure to _configure_bird_routers

--- tests/20-rip/redistribute_only_default/test_rip_redistribute_only_default.py
+++ tests/20-rip/redistribute_only_default/test_rip_redistribute_only_default.py
@@ -35,7 +35,7 @@ class TestRIPRedistributeOnlyDefault(BirdPlanBaseTestCase):
 
     def test_configure(self, sim, tmpdir):
         """Create our configuration files."""
-        self._test_configure(sim, tmpdir)
+        self._configure_bird_routers(sim, tmpdir)
 
     def test_create_topology(self, sim, tmpdir):
         """Test topology creation."""
