ee2e924e8495c2e4e5baff637274634943804706
Nigel Kukard <nkukard@lbsd.net>
2020-10-19 12:59:33 +0000

    birdplan/bgp: Fixed syntax issues

--- birdplan/bird_config/sections/protocols/bgp/__init__.py
+++ birdplan/bird_config/sections/protocols/bgp/__init__.py
@@ -232,8 +232,8 @@ class ProtocolBGP(SectionProtocolBase):  # pylint: disable=too-many-public-metho
         self.constants.conf.append("define BGP_LC_STRIP = [ ")
         self.constants.conf.append("  (BGP_ASN, 1..3, *),")  # Strip route learned functions
         # Allow client traffic engineering: 4, 5, 6, 7, 8
-        self.constants.conf.append("  (BGP_ASN, 9..61, *),") # Strip unused
-        self.constants.conf.append("  (BGP_ASN, 64..71, *),") # Strip unused
+        self.constants.conf.append("  (BGP_ASN, 9..61, *),")  # Strip unused
+        self.constants.conf.append("  (BGP_ASN, 64..71, *),")  # Strip unused
         self.constants.conf.append("  (BGP_ASN, 74..65535, *),")  # Strip unsed + rest (incl. 1000 - info, 1101 - filter)
         # These functions should never be used on our own ASN
         self.constants.conf.append("  (BGP_ASN, 4, BGP_ASN),")
