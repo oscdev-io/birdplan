1296893117f70269699b5b4db1e1f0f44ac93dcc
Nigel Kukard <nkukard@lbsd.net>
2021-08-19 16:32:03 +0000

    tests: Disable BirdClient debug flag

--- tests/basetests.py
+++ tests/basetests.py
@@ -113,7 +113,7 @@ class BirdPlanBaseTestCase:
         # Configure our simulator with the BIRD routers
         configured_routers = self._configure_bird_routers(sim, tmpdir)
         for router in configured_routers:
-            sim.add_node(BirdRouterNode(name=router, configfile=f"{tmpdir}/bird.conf.{router}", debug=True))
+            sim.add_node(BirdRouterNode(name=router, configfile=f"{tmpdir}/bird.conf.{router}", debug=False))
 
         # Loop with our ExaBGP's and create the nodes
         self._configure_exabgps(sim, tmpdir)
