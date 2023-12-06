11a0a57e92c08c368bd2a8747e7efc6bf737b82b
Nigel Kukard <nkukard@lbsd.net>
2023-12-06 06:25:24 +0000

    fix: OSPF commandline interface must use the new API, don't use live data, use cahced/generated instead

--- birdplan/plugins/cmdline/ospf/interface/show.py
+++ birdplan/plugins/cmdline/ospf/interface/show.py
@@ -21,6 +21,7 @@
 import argparse
 from typing import Any, Dict, List
 
+from .....cmdline import BirdPlanCommandLine
 from .....console.colors import colored
 from ...cmdline_plugin import BirdPlanCmdlinePluginBase
 
@@ -92,10 +93,13 @@ class BirdplanCmdlineOSPFInterfaceShow(BirdPlanCmdlinePluginBase):
         if not self._subparser:
             raise RuntimeError()
 
-        cmdline = args["cmdline"]
+        cmdline: BirdPlanCommandLine = args["cmdline"]
 
-        # Load BirdPlan configuration
-        cmdline.birdplan_load_config(use_cached=True)
+        # Suppress info output
+        cmdline.birdplan.birdconf.birdconfig_globals.suppress_info = True
+
+        # Load BirdPlan configuration using the cache
+        cmdline.birdplan_load_config(ignore_irr_changes=True, ignore_peeringdb_changes=True, use_cached=True)
 
         # Grab peer list
         return cmdline.birdplan.state_ospf_interface_status()
