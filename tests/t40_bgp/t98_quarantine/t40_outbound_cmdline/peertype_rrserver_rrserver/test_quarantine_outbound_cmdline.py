a02be82c3a05e0971da78f05a854a79e354dd6c2
Nigel Kukard <nkukard@lbsd.net>
2025-03-04 00:28:23 +0000

    chore: updated copyright to 2025

--- tests/t40_bgp/t98_quarantine/t40_outbound_cmdline/peertype_rrserver_rrserver/test_quarantine_outbound_cmdline.py
+++ tests/t40_bgp/t98_quarantine/t40_outbound_cmdline/peertype_rrserver_rrserver/test_quarantine_outbound_cmdline.py
@@ -1,7 +1,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# Copyright (c) 2019-2024, AllWorldIT
+# Copyright (c) 2019-2025, AllWorldIT
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
